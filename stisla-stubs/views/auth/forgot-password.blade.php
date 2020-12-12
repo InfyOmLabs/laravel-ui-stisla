06a941a34ad2a9ebf419f3847b8836e135d21af8
Vishal InfyOm <vishal.ribdiya@infyom.com>
2020-12-12 17:59:17 +0530

    feat: forgot password and reset password blade files added

---
+++ stisla-stubs/views/auth/forgot-password.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.auth_app')
+@section('title')
+    Forgot Password
+@endsection
+@section('content')
+    <div class="card card-primary">
+        <div class="card-header"><h4>Reset Password</h4></div>
+
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                           name="email" tabindex="1" value="{{ old('email') }}" autofocus required>
+                    <div class="invalid-feedback">
+                        {{ $errors->first('email') }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                        Send Reset Link
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="mt-5 text-muted text-center">
+        Recalled your login info? <a href="{{ route('login') }}">Sign In</a>
+    </div>
+@endsection
