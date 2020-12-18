5e45b1858cdf03dbb824aede61e414d85aa64115
Vishal InfyOm <vishal.ribdiya@infyom.com>
2020-12-18 10:25:21 +0530

    fix: use normal form tags instead of laravel html collective

--- stisla-stubs/views/profile/edit_profile.blade.php
+++ stisla-stubs/views/profile/edit_profile.blade.php
@@ -6,25 +6,25 @@
                 <h5 class="modal-title">Edit Profile</h5>
                 <button type="button" aria-label="Close" class="close outline-none" data-dismiss="modal">×</button>
             </div>
-            {{ Form::open(['id'=>'editProfileForm','files'=>true]) }}
+            <form method="POST" id="editProfileForm" enctype="multipart/form-data">
             <div class="modal-body">
                 <div class="alert alert-danger d-none" id="editProfileValidationErrorsBox"></div>
-                {{ Form::hidden('user_id',null,['id'=>'pfUserId']) }}
-                {{ Form::hidden('is_active',1) }}
+                <input type="hidden" name="user_id" id="pfUserId">ð
+                <input type="hidden" name="is_active" value="1">
                 {{csrf_field()}}
                 <div class="row">
                     <div class="form-group col-sm-6">
-                        {{ Form::label('name', 'Name :') }}<span class="required">*</span>
-                        {{ Form::text('name', null, ['id'=>'pfName','class' => 'form-control','required', 'autofocus', 'tabindex' => "1"]) }}
+                        <label>Name:</label><span class="required">*</span>
+                        <input type="text" name="name" id="pfName" class="form-control" required autofocus tabindex="1">
                     </div>
                     <div class="form-group col-sm-6 d-flex">
                         <div class="col-sm-4 col-md-6 pl-0 form-group">
-                            {{ Form::label('photo', 'Profile Image :') }}
+                            <label>Profile Image:</label>
                             <br>
                             <label
                                     class="image__file-upload btn btn-primary text-white"
                                     tabindex="2"> Choose
-                                {{ Form::file('photo',['id'=>'pfImage','class' => 'd-none']) }}
+                                <input type="file" name="photo" id="pfImage" class="d-none" >
                             </label>
                         </div>
                         <div class="col-sm-3 preview-image-video-container float-right mt-1">
@@ -35,18 +35,18 @@
                 </div>
                 <div class="row">
                     <div class="form-group col-sm-6">
-                        {{ Form::label('email','Email :') }}<span class="required">*</span>
-                        {{ Form::text('email', null, ['id'=>'pfEmail','class' => 'form-control','required', 'tabindex' => "3"]) }}
+                        <label>Email:</label><span class="required">*</span>
+                        <input type="text" name="email" id="pfEmail" class="form-control" required tabindex="3">
                     </div>
                 </div>
                 <div class="text-right">
-                    {{ Form::button('save', ['type'=>'submit','class' => 'btn btn-primary','id'=>'btnPrEditSave','data-loading-text'=>"<span class='spinner-border spinner-border-sm'></span> Processing...", 'tabindex' => "5"]) }}
+                    <button type="submit" class="btn btn-primary" id="btnPrEditSave" data-loading-text="<span class='spinner-border spinner-border-sm'></span> Processing..." tabindex="5">Save</button>
                     <button type="button" class="btn btn-light ml-1 edit-cancel-margin margin-left-5"
                             data-dismiss="modal">Cancel
                     </button>
                 </div>
             </div>
-            {{ Form::close() }}
+            </form>>
         </div>
     </div>
 </div>
