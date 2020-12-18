5f51d3dbc5867e7b7beed612a2057ddbeb3c09ed
Vishal InfyOm <vishal.ribdiya@infyom.com>
2020-12-18 11:42:31 +0530

    fix: blade templates updated

--- stisla-stubs/views/profile/edit_profile.blade.php
+++ stisla-stubs/views/profile/edit_profile.blade.php
@@ -7,46 +7,46 @@
                 <button type="button" aria-label="Close" class="close outline-none" data-dismiss="modal">×</button>
             </div>
             <form method="POST" id="editProfileForm" enctype="multipart/form-data">
-            <div class="modal-body">
-                <div class="alert alert-danger d-none" id="editProfileValidationErrorsBox"></div>
-                <input type="hidden" name="user_id" id="pfUserId">ð
-                <input type="hidden" name="is_active" value="1">
-                {{csrf_field()}}
-                <div class="row">
-                    <div class="form-group col-sm-6">
-                        <label>Name:</label><span class="required">*</span>
-                        <input type="text" name="name" id="pfName" class="form-control" required autofocus tabindex="1">
-                    </div>
-                    <div class="form-group col-sm-6 d-flex">
-                        <div class="col-sm-4 col-md-6 pl-0 form-group">
-                            <label>Profile Image:</label>
-                            <br>
-                            <label
-                                    class="image__file-upload btn btn-primary text-white"
-                                    tabindex="2"> Choose
-                                <input type="file" name="photo" id="pfImage" class="d-none" >
-                            </label>
+                <div class="modal-body">
+                    <div class="alert alert-danger d-none" id="editProfileValidationErrorsBox"></div>
+                    <input type="hidden" name="user_id" id="pfUserId">
+                    <input type="hidden" name="is_active" value="1">
+                    {{csrf_field()}}
+                    <div class="row">
+                        <div class="form-group col-sm-6">
+                            <label>Name:</label><span class="required">*</span>
+                            <input type="text" name="name" id="pfName" class="form-control" required autofocus tabindex="1">
                         </div>
-                        <div class="col-sm-3 preview-image-video-container float-right mt-1">
-                            <img id='edit_preview_photo' class="img-thumbnail user-img user-profile-img profilePicture"
-                                 src="{{asset('img/logo.png')}}"/>
+                        <div class="form-group col-sm-6 d-flex">
+                            <div class="col-sm-4 col-md-6 pl-0 form-group">
+                                <label>Profile Image:</label>
+                                <br>
+                                <label
+                                        class="image__file-upload btn btn-primary text-white"
+                                        tabindex="2"> Choose
+                                    <input type="file" name="photo" id="pfImage" class="d-none" >
+                                </label>
+                            </div>
+                            <div class="col-sm-3 preview-image-video-container float-right mt-1">
+                                <img id='edit_preview_photo' class="img-thumbnail user-img user-profile-img profilePicture"
+                                     src="{{asset('img/logo.png')}}"/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-sm-6">
-                        <label>Email:</label><span class="required">*</span>
-                        <input type="text" name="email" id="pfEmail" class="form-control" required tabindex="3">
+                    <div class="row">
+                        <div class="form-group col-sm-6">
+                            <label>Email:</label><span class="required">*</span>
+                            <input type="text" name="email" id="pfEmail" class="form-control" required tabindex="3">
+                        </div>
+                    </div>
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary" id="btnPrEditSave" data-loading-text="<span class='spinner-border spinner-border-sm'></span> Processing..." tabindex="5">Save</button>
+                        <button type="button" class="btn btn-light ml-1 edit-cancel-margin margin-left-5"
+                                data-dismiss="modal">Cancel
+                        </button>
                     </div>
                 </div>
-                <div class="text-right">
-                    <button type="submit" class="btn btn-primary" id="btnPrEditSave" data-loading-text="<span class='spinner-border spinner-border-sm'></span> Processing..." tabindex="5">Save</button>
-                    <button type="button" class="btn btn-light ml-1 edit-cancel-margin margin-left-5"
-                            data-dismiss="modal">Cancel
-                    </button>
-                </div>
-            </div>
-            </form>>
+            </form>
         </div>
     </div>
 </div>
