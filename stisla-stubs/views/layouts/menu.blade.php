5ca429321410474a2e284873dd84e59bfbf99608
Mitul Golakiya <mtl.golakiya@gmail.com>
2022-02-21 12:39:37 +0530

    feat: Laravel 9 support added

--- stisla-stubs/views/layouts/menu.blade.php
+++ stisla-stubs/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+<li class="side-menus {{ Request::is('home') ? 'active' : '' }}">
     <a class="nav-link" href="/">
         <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
